c817256ab642cf1317cfec7a99dd8db91fcefaaa
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-07 07:29:25 -0400

    Add text article file parsing

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,6 +1,31 @@
 require 'sinatra/base'
+require 'ostruct'
+require 'time'
 
 class Blog < Sinatra::Base
   # generate absolute path
   set :root, File.expand_path('../../', __FILE__)
+
+  # loop through all articles
+  Dir.glob "#{root}/articles/*.md" do |file|
+    # parse metadata and content
+    meta, content = File.read(file).split("\n\n", 2)
+
+    # generate a metadata object
+    article = OpenStruct.new YAML.load(meta)
+
+    # convert the date to a time object
+    article.content = Time.parse article.date.to_s
+
+    # add the content
+    article.content = content
+
+    # generate the slug
+    article.slug = Filename.basename(file, '.md')
+
+    # set up the route
+    get "/#{article.slug}" do
+      erb :post, locals: { article: article }
+    end
+  end
 end
