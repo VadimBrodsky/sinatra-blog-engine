e9dba10df38056cc0d64205d9c02da44285995ed
Vadim Brodsky <vadim.brodsky@gmail.com>
2015-08-09 17:50:52 -0400

    Add github hook that will restart the app

---
+++ lib/github_hook.rb
@@ -0,0 +1,17 @@
+require 'sinatra/base'
+require 'time'
+
+class GithubHook < Sinatra::Base
+  post '/update' do
+    app.settings.reset!
+    load app.settings.app_file
+
+    content_type :txt
+    if settings.autopull?
+      # pipe to stdout to make
+      `git pull 2>&1`
+    else
+      "ok"
+    end
+  end
+end
